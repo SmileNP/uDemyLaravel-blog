0e1e5c0ecf2de1d3ce7bd8568cbca5ac9bd4049d
SmileNP <smile.bestybay@nabepero.co.jp>
2023-08-11 17:57:40 -0700

    how to delete dable entries

--- routes/web.php
+++ routes/web.php
@@ -4,33 +4,83 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers as C;
 
+use App\Models\Blog;
+
+/*
+|--------------------------------------------------------------------------
+| ELOQUENT
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/read', function () {
+
+    $posts = Blog::all();
+
+    foreach ($posts as $post) {
+        return $post->title;
+    }
+    return null;
+});
+
+Route::get('/find', function () {
+    return Blog::find(2)->title;
+});
+
+Route::get('/findwhere', function () {
+    return Blog::where('id', 3)->orderBy('id', 'desc')->take(1)->get();
+});
+
+Route::get('/insert', function () {
+    $blog = new Blog();
+
+    $blog->title = "New Eloquent Blog";
+    $blog->content = "eloquent content";
+
+    $blog->save();
+});
+
+Route::get('/create', function () {
+    Blog::create(['title' => 'the create method', 'content' => 'WOW']);
+});
+
+Route::get('/update', function () {
+    Blog::where('id', 2)->update(['title' => 'NEW UPDATED TITLE']);
+});
+
+Route::get('/delete', function () {
+//    Blog::find(2)->delete();
+//    Blog::destroy([3, 5]);
+});
+
+
+
 /*
 |--------------------------------------------------------------------------
 | DATABASE Raw SQL Queries
 |--------------------------------------------------------------------------
 */
 
-Route::get('/insert', function () {
-    DB::insert('insert into blogs(title, content) values(?,?)', ['PHP with laravel', 'Laravel is the best thing that has happened to PHP']);
-});
-
-Route::get('/read', function () {
-    $results = DB::select('select * from blogs where id = ?', [1]);
-//    $str = '';
-//    foreach ($results as $blog) {
-//        $str = $str . $blog->title . " " . $blog->content;
-//    }
-//    return 'READ ' . $str;
-    return var_dump($results);
-});
-
-Route::get('/update', function () {
-    return DB::update('update blogs set title = "Updated title" where id = ?', [1]);
-});
-
-Route::get('/delete', function () {
-    return DB::delete('delete from blogs where id = ?', [1]);
-});
+//Route::get('/insert', function () {
+//    DB::insert('insert into blogs(title, content) values(?,?)', ['DJANGO', 'Django is the best thing that has happened to python']);
+//});
+//
+//Route::get('/read', function () {
+//    $results = DB::select('select * from blogs where id = ?', [1]);
+////    $str = '';
+////    foreach ($results as $blog) {
+////        $str = $str . $blog->title . " " . $blog->content;
+////    }
+////    return 'READ ' . $str;
+//    return var_dump($results);
+//});
+//
+//Route::get('/update', function () {
+//    return DB::update('update blogs set title = "Updated title" where id = ?', [1]);
+//});
+//
+//Route::get('/delete', function () {
+//    return DB::delete('delete from blogs where id = ?', [1]);
+//});
 
 /*
 |--------------------------------------------------------------------------
